e87833a7c266408a62bb136072f347dd07df8cd3
Mehmet Ali Değirmenci <m.alidegirmenci@hotmail.com>
2023-12-21 15:11:51 +0300

    init spring boot

---
+++ src/main/java/com/workintech/FswebS18D4JavaProjesiApplication.java
@@ -0,0 +1,13 @@
+package com.workintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FswebS18D4JavaProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FswebS18D4JavaProjesiApplication.class, args);
+	}
+
+}
